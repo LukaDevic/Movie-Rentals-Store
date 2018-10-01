05bf8262769e14e635386025ca396a3ddd495496
Luka Devic <luka.m.devic@gmail.com>
2018-10-01 19:17:49 +0200

    initial commit

---
+++ MovieRentalsStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieRentalsStore.Startup))]
+namespace MovieRentalsStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
