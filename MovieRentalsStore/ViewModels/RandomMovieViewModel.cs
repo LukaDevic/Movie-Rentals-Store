69bf871bc097d15a625fa79515ec878ece7edd42
Luka Devic <luka.m.devic@gmail.com>
2018-10-02 09:37:47 +0200

    adding movies and customers controllers and views

---
+++ MovieRentalsStore/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MovieRentalsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentalsStore.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+
+    }
+}
