f952bd6c9f9f67cc08a9681805f3f2be3e4656c6
Luka Devic <luka.m.devic@gmail.com>
2018-10-05 07:09:22 +0200

    deleted code for creation of manager role, eevery next user will be normal user

--- MovieRentalsStore/Controllers/AccountController.cs
+++ MovieRentalsStore/Controllers/AccountController.cs
@@ -156,12 +156,7 @@ namespace MovieRentalsStore.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    //Temp code
-                    var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
-                    var roleManager = new RoleManager<IdentityRole>(roleStore);
-                    await roleManager.CreateAsync(new IdentityRole("CanManageMovies"));
-                    await UserManager.AddToRoleAsync(user.Id, "CanManageMoviesan");
-
+                   
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
